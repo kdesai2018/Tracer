6fc57a01f1db81d3ac38e2b77f552aa5105bcec5
Didi Zhou <didizhou@Didis-MacBook-Pro.local>
2019-09-14 14:56:51 -0500

    code with rendering issue

--- game.js
+++ game.js
@@ -1,28 +1,101 @@
-//the following if-statement decides if the browser is going to render
-//the application with WebGL or canvas
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-  type = 'canvas'
+// Global Vars
+var type = "WebGL";
+var appWidth = 512 * 2;
+var appHeight = 512 * 1.25;
+var app;
+var player1;
+
+// Initialization Function (begin on menu screen)
+function init() {
+    // Debug messages
+    console.log("init() successfully called.");
+    PIXI.utils.sayHello(type);
+    // // Render type
+    // if(!PIXI.utils.isWebGLSupported()){
+    //     type = 'canvas';
+    // }
+    // Configure App
+    app = new PIXI.Application({width: appWidth, height: appHeight});
+    app.renderer.backgroundColor = 0xfff00f;
+    app.renderer.autoResize = true;
+    // add created canvas to the html
+    document.body.appendChild(app.view);
+    // Load Images
+    PIXI.loader
+    .add('testing', 'https://en.wikipedia.org/wiki/File:Stick_figure.png'
+    )
+    .load(setup);
+    app.ticker.add(delta => gameLoop(delta))
+    requestAnimationFrame(gameLoop);
 }
 
-//this print a message on the console. use dev tools to see
-PIXI.utils.sayHello(type)
+function gameLoop (delta) {
+    console.log("woooo");
+}
 
-//Create a Pixi Application
-//There are more properties that can be altered
-//look at the documentation for PIXI.Application
-let appWidth = 512 * 2
-    appHeight = 512 * 1.25
-let app = new PIXI.Application({width: appWidth, height: appHeight});
-//app.renderer.backgroundColor = 0x061639
-app.renderer.autoResize = true
+function update() {
+    console.log("woooo");
+}
 
+function setup() {
+    // Initialize sprites
+    player1 = new PIXI.Sprite(PIXI.loader.resources['testing'].texture);
+    player1.anchor.x = 100;
+    player1.anchor.y = 100;
+    app.stage.addChild(player1);
 
-//add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view)
+    spacebar = keyboard(32);
 
-//PIXI.loader takes in paths for images and renders it so that they cna be used
-//for textures
+    spacebar.press = () => {
+      spacePressed = true
+    }
+    spacebar.release = () => {
+      spacePressed = false
+    }
+    //app.ticker.add(delta => gameLoop(delta)); //time increment
+    
+    console.log("START NOW");
+    //startGame()
+}
+
+// Keyboard Controls
+function keyboard(keyCode) {
+    let key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    // Down
+    key.downHandler = event => {
+        console.log("DOWN");
+      if (event.keyCode === key.code) {
+        if (key.isUp && key.press) key.press();
+          key.isDown = true;
+          key.isUp = false;
+        }
+        event.preventDefault();
+    };
+    // Up
+    key.upHandler = event => {
+        console.log("UP");
+      if (event.keyCode === key.code) {
+        if (key.isDown && key.release) key.release();
+          key.isDown = false;
+          key.isUp = true;
+        }
+        event.preventDefault();
+    };
+    //Attach event listeners
+    window.addEventListener(
+      "keydown", key.downHandler.bind(key), false
+    );
+    window.addEventListener(
+      "keyup", key.upHandler.bind(key), false
+    );
+    return key;
+  }
+
 /*PIXI.loader
   .add([
   ])
